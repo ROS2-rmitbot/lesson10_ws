93ceab77850ac9c8731950f7a9e981a655b0860f
Dinh-Son <vudinhso@gmail.com>
2025-10-03 20:20:10 +0700

    full robot with localization, mapping, nav

--- build/rmitbot_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/v120506/rmitbot_v4/lesson8_ws/src/rmitbot_bringup
-# Build directory: /home/v120506/rmitbot_v4/lesson8_ws/build/rmitbot_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/rmitbot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/v120506/rmitbot_v4/lesson8_ws/src/rmitbot_description
-# Build directory: /home/v120506/rmitbot_v4/lesson8_ws/build/rmitbot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
